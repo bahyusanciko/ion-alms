785dbbd1ddbaaa3f56a3896873f83c7c901c35da
Bahyu Sanciko <41245088+bahyusanciko@users.noreply.github.com>
2022-10-30 07:23:04 +0700

    count alms

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,40 @@
-import { IonButton, IonCard, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCard, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonPage, IonRow, IonTitle, IonToolbar, useIonAlert } from '@ionic/react';
 import { cardOutline, cashOutline, walletOutline } from 'ionicons/icons';
+import { useState } from 'react';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+  const [alert] = useIonAlert();
+  const [salary, setSalary] = useState<any>();
+  const [income, setIncome] = useState<any>();
+  const [debit, setDebit] = useState<any>();
+  const [totalAlms, setTotalAlms] = useState<any>(0);
+  const [totalSalary, setTotalSalary] = useState<any>(0);
+
+  const massageAlert = async (status:string,body:string) => {
+     alert({
+      header: status,
+      message: body,
+      buttons: [{cssClass: 'alert-button-confirm', text: 'Ok', handler: (d) => console.log('ok pressed') }],
+    })
+  }
+  const countAlms = () => {
+    if (!salary || !income || !debit) {
+        massageAlert('Gagal','Please enter a valid input')
+        return;
+    }
+    let total = parseInt(salary)+parseInt(income)-parseInt(debit);
+    setTotalSalary(total)
+    setTotalAlms((total*2.5)/100)
+  }
+
+  const Rupiah = (number:number) => {
+    return new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: "IDR"
+    }).format(Math.round(number));
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -21,7 +53,7 @@ const Tab1: React.FC = () => {
                     icon={walletOutline}
                     />
                     <IonLabel position="floating">Gaji PerBulan</IonLabel>
-                  <IonInput type="number" placeholder=" Masukan Jumlah Gaji" required />
+                  <IonInput type="number" placeholder=" Masukan Jumlah Gaji" value={salary} required onIonChange={(e:any) => setSalary(e.detail.value!)} />
                 </IonItem>
                 <IonItem>
                   <IonIcon 
@@ -29,7 +61,7 @@ const Tab1: React.FC = () => {
                     icon={cardOutline}
                     />
                   <IonLabel position="floating">Pendapatan Lainnya</IonLabel>
-                  <IonInput type="number" placeholder=" Masukan Jumlah Pendapatan Lainnya" required/>
+                  <IonInput type="number" placeholder=" Masukan Jumlah Pendapatan Lainnya" value={income} required onIonChange={(e:any) => setIncome(e.detail.value!)}/>
                 </IonItem>
                 <IonItem>
                   <IonIcon 
@@ -37,15 +69,21 @@ const Tab1: React.FC = () => {
                     icon={cashOutline}
                     />
                   <IonLabel position="floating">Hutang / Cicilan</IonLabel>
-                  <IonInput type="number" placeholder=" Masukan Jumlah Hutang / Cicilan" required/>
+                  <IonInput type="number" placeholder=" Masukan Jumlah Hutang / Cicilan" value={debit} required onIonChange={(e:any) => setDebit(e.detail.value!)}/>
                 </IonItem>
-                <IonButton expand="block" type="submit" >
+                <IonButton expand="block" type="submit" onClick={countAlms}>
                   HITUNG ZAKAT
                 </IonButton>
               </IonCol>
             </IonRow>
           </IonGrid>
         </IonCard>
+        {totalAlms > 0 &&
+          <IonCard>
+            <IonTitle>Total Penghasilan :  <br /> <b>{Rupiah(totalSalary)}</b></IonTitle>
+            <IonTitle>Jumlah zakat penghasilan Anda: <br /><b>{Rupiah(totalAlms)}</b></IonTitle>
+          </IonCard>
+        }
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,7 +1,38 @@
-import { IonButton, IonCard, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCard, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonRow, IonTitle, IonToolbar, useIonAlert } from '@ionic/react';
+import { useState } from 'react';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
+  const [alert] = useIonAlert();
+  const [salary, setSalary] = useState<any>();
+  const [income, setIncome] = useState<any>();
+  const [debit, setDebit] = useState<any>();
+  const [totalAlms, setTotalAlms] = useState<any>(0);
+  const [totalSalary, setTotalSalary] = useState<any>(0);
+
+  const massageAlert = async (status:string,body:string) => {
+     alert({
+      header: status,
+      message: body,
+      buttons: [{cssClass: 'alert-button-confirm', text: 'Ok', handler: (d) => console.log('ok pressed') }],
+    })
+  }
+  const countAlms = () => {
+    if (!salary || !income || !debit) {
+        massageAlert('Gagal','Please enter a valid input')
+        return;
+    }
+    let total = parseInt(salary)+parseInt(income)-parseInt(debit);
+    setTotalSalary(total)
+    setTotalAlms((total*2.5)/100)
+  }
+  const Rupiah = (number:number) => {
+    return new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: "IDR"
+    }).format(Math.round(number));
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,23 +47,29 @@ const Tab2: React.FC = () => {
               <IonCol class="ion-align-self-center" size-md="12" size-lg="12" size-xs="12">
                 <IonItem>
                     <IonLabel position="floating">Nilai Deposito/Tabungan/Giro</IonLabel>
-                  <IonInput type="number" placeholder=" Masukan Jumlah Nilai Deposito" required />
+                  <IonInput type="number" placeholder=" Masukan Jumlah Nilai Deposito" value={salary} required onIonChange={(e:any) => setSalary(e.detail.value!)} />
                 </IonItem>
                 <IonItem>
                   <IonLabel position="floating">Nilai Properti & Kendaraan</IonLabel>
-                  <IonInput type="number" placeholder=" Masukan number Baru" required/>
+                  <IonInput type="number" placeholder=" Masukan number Baru" value={income} required onIonChange={(e:any) => setIncome(e.detail.value!)}/>
                 </IonItem>
                 <IonItem>
                   <IonLabel position="floating">Hutang / Cicilan</IonLabel>
-                  <IonInput type="number" placeholder=" Masukan Ulangi number" required/>
+                  <IonInput type="number" placeholder=" Masukan Ulangi number" value={debit} required onIonChange={(e:any) => setDebit(e.detail.value!)}/>
                 </IonItem>
-                <IonButton expand="block" type="submit" >
+                <IonButton expand="block" type="submit" onClick={countAlms}>
                   HITUNG ZAKAT
                 </IonButton>
               </IonCol>
             </IonRow>
           </IonGrid>
         </IonCard>
+        {totalAlms > 0 &&
+          <IonCard>
+            <IonTitle>Total Penghasilan :  <br /> <b>{Rupiah(totalSalary)}</b></IonTitle>
+            <IonTitle>Jumlah zakat penghasilan Anda: <br /><b>{Rupiah(totalAlms)}</b></IonTitle>
+          </IonCard>
+        }
       </IonContent>
     </IonPage>
   );

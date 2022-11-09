aba626d5a14625af35274487ddc80f98b54d148d
Bahyu Sanciko <41245088+bahyusanciko@users.noreply.github.com>
2022-11-09 19:11:08 +0700

    name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'ion-alms',
+  appName: 'Kalkulator Zakat',
   webDir: 'build',
   bundledWebRuntime: false
 };
